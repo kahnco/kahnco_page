1fa313c519eee8d51bf572e64eba1351f2fe8b94
kahnco <ggj@fleeklounge.com>
2024-09-12 13:13:29 +0900

    chore: github-pages 세팅

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,8 @@ const nextConfig = {
       process.env.NODE_ENV === "production"
           ? "https://kahnco.github.io/kahnco_page"
           : "",
+  output: 'export', // This tells Next.js to export as static files
+  distDir: 'out',   // The output folder where the static files will be generated
 };
 
 export default nextConfig;
